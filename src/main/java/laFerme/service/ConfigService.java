cc31697ddfcabbf7cdcdefe62b5f69026a25e176
vfiatte <valentin.fiatte@gmail.com>
2016-03-15 14:56:12 +0100

    Site de Streaming

--- src/main/java/laFerme/service/ConfigService.java
+++ src/main/java/laFerme/service/ConfigService.java
@@ -6,6 +6,8 @@
 package laFerme.service;
 
 import javax.servlet.http.HttpServletRequest;
+import laFerme.entity.Personnage;
+import laFerme.entity.Ressource;
 import laFerme.entity.Utilisateur;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -19,10 +21,25 @@ public class ConfigService {
 
     @Autowired
     UtilisateurService utilisateurService;
+    
+    @Autowired
+    PersonnageService personnageService;
 
     public Utilisateur recupererUtilisateur(HttpServletRequest req) {
         Utilisateur u = utilisateurService.findOneByLogin(req.getSession().getAttribute("user").toString());
         return u;
+    }
+    
+    public void calculPoints(Personnage p){
+        int pointBle = p.getRessource().getRessourceBle()*1;
+        int pointCarotte = p.getRessource().getRessourceCarotte()*2;
+        int pointChevre = p.getRessource().getRessourceChevre()*4;
+        
+        int totalPoints = pointBle + pointCarotte + pointChevre;
+        
+        p.setNbDePoints(totalPoints);
+        personnageService.save(p);
+        
     }
 
 }
