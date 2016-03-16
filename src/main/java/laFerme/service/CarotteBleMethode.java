b949c28fb790a583912dc47da816e625cfab1673
leabonjean <lely40@hotmail.fr>
2016-03-16 14:54:23 +0100

    test html

---
+++ src/main/java/laFerme/service/CarotteBleMethode.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package laFerme.service;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.List;
+import laFerme.entity.Carotte;
+import laFerme.entity.EtatEnumeration;
+import laFerme.entity.Personnage;
+import laFerme.entity.ble;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author admin
+ */
+@Service
+public class CarotteBleMethode {
+
+    @Autowired
+    CarotteService carotteService;
+
+    @Autowired
+    BleService bleService;
+
+    @Autowired
+    RessourceService ressourceService;
+
+    public void planter(Personnage p, Class classe, Integer quantite) {
+        GregorianCalendar ajd = new GregorianCalendar();
+        if (classe.equals(Carotte.class)) {
+            List<Carotte> listeCarotte = carotteService.findAllByEtatAndPersonnageId(EtatEnumeration.NONPLANTE, p.getId());
+            if (listeCarotte.size() == 0) {
+                return;
+            } else {
+                Carotte c = listeCarotte.get(listeCarotte.size() - 1);
+                c.setEtat(EtatEnumeration.PLANTE);
+                c.setDatePlantation(ajd.getTime());
+                p.getRessource().setRessourceCarotte(p.getRessource().getRessourceCarotte() - 1);
+                carotteService.save(c);
+            }
+
+        }
+        if (classe.equals(ble.class)) {
+            List<ble> listeBle = bleService.findAllByEtatAndPersonnageId(EtatEnumeration.NONPLANTE, p.getId());
+            if (listeBle.size() == 0) {
+                return;
+            } else {
+                ble b = listeBle.get(listeBle.size() - 1);
+                b.setEtat(EtatEnumeration.PLANTE);
+                b.setDatePlantation(ajd.getTime());
+                p.getRessource().setRessourceBle(p.getRessource().getRessourceBle()- 1);
+                bleService.save(b);
+            }
+
+        }
+        ressourceService.save(p.getRessource());
+    }
+
+}
---
+++ src/main/java/laFerme/servlet/PlanterServlet.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package laFerme.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import laFerme.entity.Carotte;
+import laFerme.entity.Chevre;
+import laFerme.entity.EtatEnumeration;
+import laFerme.entity.Fromage;
+import laFerme.entity.Personnage;
+import laFerme.entity.Utilisateur;
+import laFerme.entity.ble;
+import laFerme.service.BleService;
+import laFerme.service.CarotteBleMethode;
+import laFerme.service.CarotteService;
+import laFerme.service.ConfigService;
+import laFerme.service.PersonnageService;
+import laFerme.spring.AutowireServlet;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ *
+ * @author admin
+ */
+@WebServlet(name = "PlanterServlet", urlPatterns = {"/planterRessourcesServlet"})
+public class PlanterServlet extends AutowireServlet {
+
+    @Autowired
+    PersonnageService personnageService;
+    @Autowired
+    CarotteBleMethode carottebleMethode;
+    @Autowired
+    CarotteService carotteService;
+    @Autowired
+    BleService bleService;
+    @Autowired
+    ConfigService config;
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        
+        Utilisateur u = config.recupererUtilisateur(req);
+
+        Long id = Long.parseLong(req.getParameter("idPersonnage"));
+        Personnage p = personnageService.findOne(id);
+        String c = req.getParameter("planteRessource");
+        Integer qte = Integer.parseInt(req.getParameter("quantite"));
+        if (c.equals("carotte")) {
+            carottebleMethode.planter(p, Carotte.class, qte);
+            System.out.println("je mange des carottes");
+        }
+        if (c.equals("ble")) {
+            carottebleMethode.planter(p, ble.class, qte);
+        }
+        req.setAttribute("monPersonnage", p);
+
+        List<Carotte> mesCarottes = carotteService.findAllByEtatAndPersonnageId(EtatEnumeration.PLANTE, p.getId());
+        List<ble> mesBles = bleService.findAllByEtatAndPersonnageId(EtatEnumeration.PLANTE, p.getId());
+        req.setAttribute("mesCarottesPlantees", mesCarottes.size());
+        req.setAttribute("mesBlesPlantes", mesBles.size());
+
+        req.getRequestDispatcher("PageAccueilDeMonPersonnage.jsp").include(req, resp);
+
+    }
+
+}
